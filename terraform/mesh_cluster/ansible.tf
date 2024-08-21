457e5beff63ec1b10ebba96ff151d0b2227a985c
Willard Nilges <willardnilges@gmail.com>
2024-08-20 21:46:41 -0400

    Ansible is magic

--- terraform/mesh_cluster/ansible.tf
+++ terraform/mesh_cluster/ansible.tf
@@ -6,7 +6,7 @@ resource "ansible_group" "mgrs" {
     ansible_ssh_private_key_file = "../terraform/${path.module}/mesh${var.mesh_env_name}"
     ansible_ssh_common_args      = "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
     K3S_TOKEN                    = var.k3s_token
-    DATADOG_API_KEY              = var.datadog_api_key
+    DATADOG_API_KEY              = var.DATADOG_API_KEY
   }
 }
 
