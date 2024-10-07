e20dad1cd42a62c7dd61e28df317c2e80ff9d251
james-otten <jamesotten1@gmail.com>
2024-10-06 22:44:42 -0400

    abc

--- terraform/mesh_cluster/lb.tf
+++ terraform/mesh_cluster/lb.tf
@@ -46,7 +46,7 @@ resource "proxmox_vm_qemu" "k8s-lb" {
     type = "socket"
   }
 
-  tags = "mesh${var.mesh_env_name},managed_by_iac"
+  tags = "managed_by_iac,mesh${var.mesh_env_name}"
 
   lifecycle {
     ignore_changes = [
